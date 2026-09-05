ce4c560ec131373cf913a5ac524329b1343e3757
Nada Elmasry <inada7680@gmail.com>
2026-09-05 18:31:32 +0300

    #Final upload after editing

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 from dataForm import tables
 from emplyees import router as employees_router
 from routers import attendance, auth, reg
-
 
 tables()
 
@@ -23,7 +24,8 @@ app.include_router(auth.router)
 app.include_router(attendance.router)
 app.include_router(employees_router)
 
+app.mount("/static", StaticFiles(directory="../Frontend"), name="static")
 
 @app.get("/")
-def home():
-    return {"message": "Face Recognition Attendance API is running"}
+def read_root():
+    return FileResponse("../Frontend/index.html")
